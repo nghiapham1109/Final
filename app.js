9f32c2b6c3c6f08f24212f4fe42104d6f6e95421
nghiapham1109 <nghiaphma1109@gmail.com>
2022-05-27 00:13:55 +0700

    "update27/5"

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
+//
 const patientRouter = require("./api/patient/patientRouter");
+//
 const doctorRouter = require("./api/doctor/doctorRouter");
+//
+app.use(cors());
 app.use(express.json());
+//
 app.use("/api/patient", patientRouter);
+//
 app.use("/api/doctor", doctorRouter);
+//
 app.listen(process.env.APP_PORT, () => {
   console.log("Server is running...", process.env.APP_PORT);
 });
